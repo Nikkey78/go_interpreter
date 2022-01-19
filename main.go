a97f3d1675d73385c3b4204174fccbab17334e38
Nikkey78 <nick-bazh@yandex.ru>
2022-01-19 15:37:42 +0300

    start of a REPL

--- main.go
+++ main.go
@@ -3,10 +3,31 @@ package main
 import (
 	"console"
 	"fmt"
+	"interpreter/repl"
+	"os"
+	"os/signal"
+	"os/user"
 )
 
 func main() {
 	defer console.GreenReset()()
+	go stopApp()
 
-	fmt.Println("dfsdfsdfsfsdf")
+	user, err := user.Current()
+	if err != nil {
+		console.ColorPrintln(console.Color_Red, err)
+		return
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Println("Feel free to type in commands")
+
+	repl.Start(os.Stdin, os.Stdout)
+}
+
+func stopApp() {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+	console.ResetColor()
+	os.Exit(0)
 }
